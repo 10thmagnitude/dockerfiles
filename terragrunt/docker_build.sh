b0cd7447c0ea7fbf3ed4a31a91b51b48fbc5067d
Ryan Lee <ryan@ryanl.ee>
2017-01-10 01:45:21 -0600

    Make curl silent, add log

--- terragrunt/docker_build.sh
+++ terragrunt/docker_build.sh
@@ -1,5 +1,5 @@
-set -eu
-set -o pipefail
+set -euo pipefail
 
-curl -L https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_386 -o /bin/terragrunt
+echo "Adding Terragrunt ${TERRAGRUNT_VERSION}"
+curl -sL https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_386 -o /bin/terragrunt
 chmod +x /bin/terragrunt
