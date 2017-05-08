b1841081dabdb3651a1f9338a3342506f0458673
Scott Nowicki <scott@sgn-mbp>
2017-05-08 13:40:55 -0500

    initial commit; ubuntu + terraform + powershell

---
+++ terraform-ps/install-terraform.sh
@@ -0,0 +1,21 @@
+
+set -eu
+set -o pipefail
+
+apt update
+apt install -y git curl openssh-client unzip
+
+cd $HOME
+curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip > terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+echo "${TERRAFORM_SHA256SUM}  terraform_${TERRAFORM_VERSION}_linux_amd64.zip" > terraform_${TERRAFORM_VERSION}_SHA256SUMS
+
+sha256sum -c --quiet terraform_${TERRAFORM_VERSION}_SHA256SUMS
+
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /bin
+rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+apt remove -y --purge git curl openssh-client unzip
+apt autoremove -y
+
+rm -rf /var/cache/apt/*
